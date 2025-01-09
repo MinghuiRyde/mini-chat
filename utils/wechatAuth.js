8d085361e3b479fd754c9e185900ccf5b7daca31
ZMinghuiZ <zmh992545@gmail.com>
2025-01-09 17:58:47 +0800

    Remove extra console log

--- utils/wechatAuth.js
+++ utils/wechatAuth.js
@@ -5,11 +5,8 @@ const getSessionKeyAndOpenId = async (jsCode) => {
   const appSecret = process.env.WECHAT_APP_SECRET;
   const url =
     `https://api.weixin.qq.com/sns/jscode2session?appid=${appId}&secret=${appSecret}&js_code=${jsCode}&grant_type=authorization_code`
-
-  console.log(url);
-
+  
   const response = await axios.get(url);
-  console.log('Response ', response.data);
 
   if (response.data.errorcode) {
     throw new Error(`WeChat API Error: ${response.data.errmsg}`);
