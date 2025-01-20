5c0ebb3f2a65a554df4c4925d73dbb6bb74f62f2
ZMinghuiZ <zmh992545@gmail.com>
2025-01-20 17:10:16 +0800

    Fix routing issue

--- routes/chats.js
+++ routes/chats.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middlewares/auth');
-const {getChatsByUser, createChat, updateReadStatus, updateDeliveredStatus}
-  = require('../controllers/chatsController');
+const {getChatsByUser, createChat, updateReadStatus} =
+  require('../controllers/chatsController');
 
 router.get('/:user_id', authMiddleware, getChatsByUser);
 router.post('/create', authMiddleware, createChat);
 router.post('/updateReadStatus', authMiddleware, updateReadStatus);
-router.post('/updateDeliveredStatus', authMiddleware, updateDeliveredStatus);
 
 module.exports = router;
